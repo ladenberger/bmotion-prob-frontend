670181b25217413ce476f0e12f610e0fad87de6e
Lukas Ladenberger <lukas.ladenberger@googlemail.com>
2015-09-07 10:33:42 +0200

    don't set prob home path!!

--- app/js/main/prob.standalone.js
+++ app/js/main/prob.standalone.js
@@ -157,7 +157,8 @@ define(['socketio', 'angularAMD', 'bms.func', 'bms.manifest', 'bms.config', 'ang
                     bmsModalService.setMessage("Start BMotion for ProB Server ...");
                     var spawn = require('child_process').spawn;
                     var separator = process.platform === 'win32' ? ';' : ':';
-                    var server = spawn('java', ['-Xmx1024m', '-cp', './libs/libs/*' + separator + './libs/bmotion-prob-standalone.jar', "-Dprob.home=./cli/", 'Start', '-standalone', '-local']);
+                    //var server = spawn('java', ['-Xmx1024m', '-cp', './libs/libs/*' + separator + './libs/bmotion-prob-standalone.jar', "-Dprob.home=./cli/", 'Start', '-standalone', '-local']);
+                    var server = spawn('java', ['-Xmx1024m', '-cp', './libs/libs/*' + separator + './libs/bmotion-prob-standalone.jar', 'Start', '-standalone', '-local']);
                     server.stdout.on('data', function (data) {
                         try {
                             var json = JSON.parse(data.toString('utf8'));
